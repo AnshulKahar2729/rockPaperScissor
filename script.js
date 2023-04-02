8c4478261728e3a428ea65d472456dbc1b4f2f50
Anshul Kahar <60067250+AnshulKahar2729@users.noreply.github.com>
2023-04-02 11:09:44 +0530

    script.js
    Instead of repeating the code again and again, created a function and call it according to player and computer move.

--- script.js
+++ script.js
@@ -9,147 +9,100 @@
 const rockBtn = document.getElementById("rock-btn");
 const paperBtn = document.getElementById("paper-btn");
 const scissorBtn = document.getElementById("scissor-btn");
-const winCountText = document.getElementById("winCountText")
-const lossCountText = document.getElementById("lossCountText")
-const tieCountText = document.getElementById("tieCountText")
+const winCountText = document.getElementById("winCountText");
+const lossCountText = document.getElementById("lossCountText");
+const tieCountText = document.getElementById("tieCountText");
 let youChoose = document.getElementById("youChoose");
 let compChoose = document.getElementById("compChoose");
 let result = document.getElementById("result");
 
-let randNum = Math.random();
 let winCount = 0;
 let lossCount = 0;
 let tieCount = 0;
+let compSelect = "";
 
-rockBtn.addEventListener("click", function (){
+function pickComputerMove() {
+  const randNum = Math.random();
 
-    youChoose.innerText = "ROCK";
+  // 0 to 1/3 --> ROCK
+  // 1/3 TO 2/3 --> PAPER
+  // 2/3 TO 3/3 --> SCISSOR
 
-    let compSelect = "";
-    // 0 to 1/3 --> ROCK
-    // 1/3 TO 2/3 --> PAPER
-    // 2/3 TO 3/3 --> SCISSOR
+  if (randNum >= 0 && randNum < 1 / 3) {
+    compSelect = "ROCK";
+    compChoose.innerText = compSelect;
+  } else if (randNum >= 1 / 3 && randNum < 2 / 3) {
+    compSelect = "PAPER";
+    compChoose.innerText = compSelect;
+  } else if (randNum >= 2 / 3 && randNum <= 1) {
+    compSelect = "SCISSOR";
+    compChoose.innerText = compSelect;
+  }
+}
 
-    if (randNum>=0 && randNum <1/3) {
-        compSelect = "ROCK";
-        compChoose.innerText = compSelect;
+function playGame(playerMove) {
+  if (playerMove === "ROCK") {
+    youChoose.innerText = playerMove;
+
+    if (compSelect === "ROCK") {
+      tieCount += 1;
+      tieCountText.innerText = tieCount;
+      result.innerText = "TIE";
+    } else if (compSelect === "PAPER") {
+      lossCount += 1;
+      lossCountText.innerText = lossCount;
+      result.innerText = "YOU LOSS";
+    } else if (compSelect === "SCISSOR") {
+      winCount += 1;
+      winCountText.innerText = winCount;
+      result.innerText = "YOU WIN";
     }
+  } else if (playerMove === "PAPER") {
+    youChoose.innerText = playerMove;
 
-    else if (randNum >= 1 / 3 && randNum < 2 / 3) {
-        compSelect = "PAPER";
-        compChoose.innerText = compSelect;
+    if (compSelect === "ROCK") {
+      winCount += 1;
+      winCountText.innerText = winCount;
+      result.innerText = "YOU WIN";
+    } else if (compSelect === "PAPER") {
+      tieCount += 1;
+      tieCountText.innerText = tieCount;
+      result.innerText = "TIE";
+    } else if (compSelect === "SCISSOR") {
+      lossCount += 1;
+      lossCountText.innerText = lossCount;
+      result.innerText = "YOU LOSS";
     }
+  } else if (playerMove === "SCISSOR") {
+    youChoose.innerText = playerMove;
 
-    else if (randNum >= 2 / 3 && randNum < 1) {
-        compSelect = "SCISSOR";
-        compChoose.innerText = compSelect;
+    if (compSelect === "ROCK") {
+      lossCount += 1;
+      lossCountText.innerText = lossCount;
+      result.innerText = "YOU LOSS";
+    } else if (compSelect === "PAPER") {
+      winCount += 1;
+      winCountText.innerText = winCount;
+      result.innerText = "YOU LOSS";
+    } else if (compSelect === "SCISSOR") {
+      tieCount += 1;
+      tieCountText.innerText = tieCount;
+      result.innerText = "TIE";
     }
+  }
+}
 
-    if(compSelect === "ROCK"){
-        tieCount += 1;
-        tieCountText.innerText = tieCount;
-        randNum = Math.random();
-        result.innerText = "TIE";
-    }
-    else if (compSelect === "PAPER"){
-        lossCount += 1;
-        lossCountText.innerText = lossCount;
-        randNum = Math.random();
-        result.innerText = "YOU LOSS";
-    }
-    else if(compSelect === "SCISSOR"){
-        winCount += 1;
-        winCountText.innerText = winCount;
-        randNum = Math.random();
-        result.innerText = "YOU WIN";
-    }
-})
+rockBtn.addEventListener("click", function () {
+  pickComputerMove();
+  playGame("ROCK");
+});
 
-paperBtn.addEventListener("click", function (){
+paperBtn.addEventListener("click", function () {
+  pickComputerMove();
+  playGame("PAPER");
+});
 
-    youChoose.innerText = "PAPER";
-
-    let compSelect = "";
-    // 0 to 1/3 --> ROCK
-    // 1/3 TO 2/3 --> PAPER
-    // 2/3 TO 3/3 --> SCISSOR
-
-    if (randNum>=0 && randNum <1/3) {
-        compSelect = "ROCK";
-        compChoose.innerText = compSelect;
-    }
-
-    else if (randNum >= 1 / 3 && randNum < 2 / 3) {
-        compSelect = "PAPER";
-        compChoose.innerText = compSelect;
-    }
-
-    else if (randNum >= 2 / 3 && randNum < 1) {
-        compSelect = "SCISSOR";
-        compChoose.innerText = compSelect;
-    }
-
-    if(compSelect === "ROCK"){
-        winCount += 1;
-        winCountText.innerText = winCount;
-        randNum = Math.random();
-        result.innerText = "YOU WIN";
-    }
-    else if (compSelect === "PAPER"){
-        tieCount += 1;
-        tieCountText.innerText = tieCount;
-        randNum = Math.random();
-        result.innerText = "TIE";
-    }
-    else if(compSelect === "SCISSOR"){
-        lossCount += 1;
-        lossCountText.innerText = lossCount;
-        randNum = Math.random();
-        result.innerText = "YOU LOSS";
-    }
-})
-
-scissorBtn.addEventListener("click", function (){
-
-    youChoose.innerText = "SCISSOR";
-
-    let compSelect = "";
-    // 0 to 1/3 --> ROCK
-    // 1/3 TO 2/3 --> PAPER
-    // 2/3 TO 3/3 --> SCISSOR
-
-    if (randNum>=0 && randNum <1/3) {
-        compSelect = "ROCK";
-        compChoose.innerText = compSelect;
-    }
-
-    else if (randNum >= 1 / 3 && randNum < 2 / 3) {
-        compSelect = "PAPER";
-        compChoose.innerText = compSelect;
-    }
-
-    else if (randNum >= 2 / 3 && randNum < 1) {
-        compSelect = "SCISSOR";
-        compChoose.innerText = compSelect;
-    }
-
-    if(compSelect === "ROCK"){
-        lossCount += 1;
-        lossCountText.innerText = lossCount;
-        randNum = Math.random();
-        result.innerText = "YOU LOSS";
-    }
-    else if (compSelect === "PAPER"){
-        winCount += 1;
-        winCountText.innerText = winCount;
-        randNum = Math.random();
-        result.innerText = "YOU LOSS";
-    }
-    else if(compSelect === "SCISSOR"){
-        tieCount += 1;
-        tieCountText.innerText = tieCount;
-        randNum = Math.random();
-        result.innerText = "TIE";
-    }
-})
-
+scissorBtn.addEventListener("click", function () {
+  pickComputerMove();
+  playGame("SCISSOR");
+});
